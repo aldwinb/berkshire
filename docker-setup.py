ba196446e03202650c871951e8b9b97a9be0624f
Aldwin Barredo <aldwinb@users.noreply.github.com>
2017-07-19 23:11:07 -0400

    Modified startup scripts (local and in Travis CI) to push dev base image first if it's updated. Modified docker-compose files to use uploaded dev base image from Docker hub. Control dev base image tagging via file (base-version.txt). Fixed bug where db init script is reading incorrect env variable.

--- docker-setup.py
+++ docker-setup.py
@@ -4,7 +4,7 @@ import os
 
 def setup_dbs():
     host = os.environ['COUCHDB_HOST']
-    activity_db_name = os.environ['APP_ACTIVITY_DATABASE']
+    activity_db_name = os.environ['COUCHDB_ACTIVITY_DB']
     server = couchdb.Server(host, )
     if activity_db_name not in server:
         server.create(activity_db_name)
