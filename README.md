71215efcf260e92c8f61f091927c687cf20ca76a
Aldwin Barredo <aldwinb@users.noreply.github.com>
2017-11-12 14:41:01 -0500

    Added support for running integration tests locally.
    Modified development environment setup scripts so it does not conflict with
    integration test setup scripts. Integration tests spin and destroy its
    environment every time it's ran. Modified README.md.

--- README.md
+++ README.md
@@ -12,3 +12,9 @@ bash bin/docker-start.bash
 ```bash
 bash bin/run-tests.bash
 ```
+
+To run integration tests locally:
+```bash
+bash bin/run-integration-tests-local.bash -p POSTMAN_ENVIRONMENT_FILE
+
+```
---
+++ bin/local-integration-test-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+set -eu -o pipefail
+
+until wget -s http://app:8081 &> /dev/null; do
+  >&2 echo "Berkshire app is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Berkshire app is up - executing command"
+
+newman run berkshire-api.postman_collection.json \
+  -e postman_environment.json \
+  -d berkshire-api.postman_data.json || exit 1
